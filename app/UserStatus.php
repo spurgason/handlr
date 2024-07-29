be05d50dd76dc2eb3955bd23fe5b5b4cec03761b
Shane <shanepurgason.98@gmail.com>
2024-07-28 20:19:09 -0400

    add user status

---
+++ app/UserStatus.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+enum UserStatus: int
+{
+    case Active = 1;
+
+    case Inactive = 0;
+
+    case Locked = 2;
+
+    public function label(): string
+    {
+        return match ($this) {
+           self::Active => 'Active',
+           self::Inactive => 'Inactive',
+           self::Locked => 'Locked',
+        };
+    }
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\UserStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('last_login')->nullable();
+            $table->integer('status')->default(UserStatus::Inactive);
             $table->rememberToken();
             $table->timestamps();
         });
